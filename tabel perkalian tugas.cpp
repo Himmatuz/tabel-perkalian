28fb6880510ac7bde2a1bb1d9855945ba39e7fe3
Himmatuz <72491258+Himmatuz@users.noreply.github.com>
2020-10-27 20:11:13 +0700

    Add files via upload

---
+++ tabel perkalian tugas.cpp
@@ -0,0 +1,29 @@
+/* Program Tabel Perkalian */
+/* Himmatuz Zahiroh */
+/* 20051397070 */
+/* 2020B */
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+
+main()
+{
+int i,j;
+cout<<"Tabel Perkalian \n";
+
+
+for(i=1;i<=10;i++)
+{cout<<" "<<i;
+
+
+for(j=1;j<=10;j++)
+cout<<" "<<i*j;
+cout<<endl;
+}
+
+getch();
+
+}
